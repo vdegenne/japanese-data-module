65ba821650ef6cb759822a7d16568a7e02141199
vdegenne <vdegenne@gmail.com>
2023-12-12 19:27:04 +0100

    before ids

--- packages/raw/src/transform/index.ts
+++ packages/raw/src/transform/index.ts
@@ -1,11 +1,21 @@
 import fs from 'fs';
+import {Kanji, Word} from '../types.js';
 
 let id = 1;
-let gap = 10;
+let gap = 50;
+
 for (const type of ['kanji', 'word']) {
 	let jlpt = 5;
 	for (; jlpt !== 0; --jlpt) {
 		const filepath = `./out/${type}-jlpt${jlpt}.json`;
-		console.log(filepath);
+		const file = await fs.promises.readFile(filepath);
+		const data = JSON.parse(file.toString()) as (Kanji | Word)[];
+		for (const item of data) {
+			item.id = id++;
+		}
+		// await fs.promises.writeFile(filepath, JSON.stringify(data))
+		id += gap;
 	}
+	// give an extra space for kanji <-> words
+	id += gap;
 }
