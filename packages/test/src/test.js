cea6b82e34b9c3a0d65e034a5b9373bd593a6fe6
vdegenne <vdegenne@gmail.com>
2023-12-13 15:19:06 +0100

    test out

--- packages/test/src/test.js
+++ packages/test/src/test.js
@@ -1,1 +1,4 @@
-import {} from 'japanese-data-module/lib/build/constants';
+import test from 'japanese-data-module/tagless-noroman/word-jlpt2.json' assert {type: 'json'};
+import {RI} from 'japanese-data-module/tagless-noroman';
+
+console.log(test[0][RI.MEANING]);
