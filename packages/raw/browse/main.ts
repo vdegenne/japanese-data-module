d0c8344c47ebe4390e0d6c9b781ffc3265f0c0bb
vdegenne <vdegenne@gmail.com>
2023-12-13 10:15:52 +0100

    test the data

--- packages/raw/browse/main.ts
+++ packages/raw/browse/main.ts
@@ -8,6 +8,16 @@ import word4 from '../out/word-jlpt4.json' assert {type: 'json'};
 import word3 from '../out/word-jlpt3.json' assert {type: 'json'};
 import word2 from '../out/word-jlpt2.json' assert {type: 'json'};
 import word1 from '../out/word-jlpt1.json' assert {type: 'json'};
+import kanji5old from '../../../src/data/jlpt5-kanjis.json' assert {type: 'json'};
+import kanji4old from '../../../src/data/jlpt4-kanjis.json' assert {type: 'json'};
+import kanji3old from '../../../src/data/jlpt3-kanjis.json' assert {type: 'json'};
+import kanji2old from '../../../src/data/jlpt2-kanjis.json' assert {type: 'json'};
+import kanji1old from '../../../src/data/jlpt1-kanjis.json' assert {type: 'json'};
+import word5old from '../../../src/data/jlpt5-words.json' assert {type: 'json'};
+import word4old from '../../../src/data/jlpt4-words.json' assert {type: 'json'};
+import word3old from '../../../src/data/jlpt3-words.json' assert {type: 'json'};
+import word2old from '../../../src/data/jlpt2-words.json' assert {type: 'json'};
+import word1old from '../../../src/data/jlpt1-words.json' assert {type: 'json'};
 
 declare global {
 	interface Window {
@@ -15,13 +25,33 @@ declare global {
 	}
 }
 
-console.log(styles);
-
 const set = {
 	kanjis: [kanji5, kanji4, kanji3, kanji2, kanji1],
 	words: [word5, word4, word3, word2, word1],
+};
+const oldset = {
+	kanjis: [kanji5old, kanji4old, kanji3old, kanji2old, kanji1old],
+	words: [word5old, word4old, word3old, word2old, word1old],
 };
 
 const randomWordsSet = set.words[Math.floor(Math.random() * 5)];
 
 window.container.textContent = JSON.stringify(randomWordsSet, null, 2);
+
+const allids = [...set.kanjis.flat(), ...set.words.flat()].map((i) => i.id);
+const allwords = [...set.kanjis.flat(), ...set.words.flat()].map((i) => i.v);
+sendDataOverNetwork(allwords);
+
+function sendDataOverNetwork(data: any) {
+	const currentDomain = window.location.origin;
+
+	fetch(`${currentDomain}`, {
+		method: 'POST',
+		body: JSON.stringify(data),
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	})
+		.then((response) => console.log('Data sent:', response))
+		.catch((error) => console.error('Error sending data:', error));
+}
--- packages/raw/src/stats/index.ts
+++ packages/raw/src/stats/index.ts
@@ -1,31 +1,59 @@
-/*import fs from 'fs';
-import {Kanji} from '../types.js';
+import fs from 'fs';
+import {Kanji, Word} from '../types.js';
 import chalk from 'chalk';
 
-import kanji5 from '../../out/kanji-jlpt5.json' assert {type: 'json'};
-import kanji4 from '../../out/kanji-jlpt4.json' assert {type: 'json'};
-import kanji3 from '../../out/kanji-jlpt3.json' assert {type: 'json'};
-import kanji2 from '../../out/kanji-jlpt2.json' assert {type: 'json'};
-import kanji1 from '../../out/kanji-jlpt1.json' assert {type: 'json'};
-import word5 from '../../out/word-jlpt5.json' assert {type: 'json'};
-import word4 from '../../out/word-jlpt4.json' assert {type: 'json'};
-import word3 from '../../out/word-jlpt3.json' assert {type: 'json'};
-import word2 from '../../out/word-jlpt2.json' assert {type: 'json'};
-import word1 from '../../out/word-jlpt1.json' assert {type: 'json'};
-import kanji5old from '../../../../src/data/jlpt5-kanjis.json' assert {type: 'json'};
-import kanji4old from '../../../../src/data/jlpt4-kanjis.json' assert {type: 'json'};
-import kanji3old from '../../../../src/data/jlpt3-kanjis.json' assert {type: 'json'};
-import kanji2old from '../../../../src/data/jlpt2-kanjis.json' assert {type: 'json'};
-import kanji1old from '../../../../src/data/jlpt1-kanjis.json' assert {type: 'json'};
-import word5old from '../../../../src/data/jlpt5-words.json' assert {type: 'json'};
-import word4old from '../../../../src/data/jlpt4-words.json' assert {type: 'json'};
-import word3old from '../../../../src/data/jlpt3-words.json' assert {type: 'json'};
-import word2old from '../../../../src/data/jlpt2-words.json' assert {type: 'json'};
-import word1old from '../../../../src/data/jlpt1-words.json' assert {type: 'json'};
+// import kanji5 from '../../out/kanji-jlpt5.json' assert {type: 'json'};
+// import kanji4 from '../../out/kanji-jlpt4.json' assert {type: 'json'};
+// import kanji3 from '../../out/kanji-jlpt3.json' assert {type: 'json'};
+// import kanji2 from '../../out/kanji-jlpt2.json' assert {type: 'json'};
+// import kanji1 from '../../out/kanji-jlpt1.json' assert {type: 'json'};
+// import word5 from '../../out/word-jlpt5.json' assert {type: 'json'};
+// import word4 from '../../out/word-jlpt4.json' assert {type: 'json'};
+// import word3 from '../../out/word-jlpt3.json' assert {type: 'json'};
+// import word2 from '../../out/word-jlpt2.json' assert {type: 'json'};
+// import word1 from '../../out/word-jlpt1.json' assert {type: 'json'};
+// import kanji5old from '../../../../src/data/jlpt5-kanjis.json' assert {type: 'json'};
+// import kanji4old from '../../../../src/data/jlpt4-kanjis.json' assert {type: 'json'};
+// import kanji3old from '../../../../src/data/jlpt3-kanjis.json' assert {type: 'json'};
+// import kanji2old from '../../../../src/data/jlpt2-kanjis.json' assert {type: 'json'};
+// import kanji1old from '../../../../src/data/jlpt1-kanjis.json' assert {type: 'json'};
+// import word5old from '../../../../src/data/jlpt5-words.json' assert {type: 'json'};
+// import word4old from '../../../../src/data/jlpt4-words.json' assert {type: 'json'};
+// import word3old from '../../../../src/data/jlpt3-words.json' assert {type: 'json'};
+// import word2old from '../../../../src/data/jlpt2-words.json' assert {type: 'json'};
+// import word1old from '../../../../src/data/jlpt1-words.json' assert {type: 'json'};
+
+async function getJSON(filepath: string) {
+	const file = await fs.promises.readFile(filepath);
+	return JSON.parse(file.toString());
+}
+
+// new
+const kanji5 = await getJSON('./out/kanji-jlpt5.json');
+const kanji4 = await getJSON('./out/kanji-jlpt4.json');
+const kanji3 = await getJSON('./out/kanji-jlpt3.json');
+const kanji2 = await getJSON('./out/kanji-jlpt2.json');
+const kanji1 = await getJSON('./out/kanji-jlpt1.json');
+const word5 = await getJSON('./out/word-jlpt5.json');
+const word4 = await getJSON('./out/word-jlpt4.json');
+const word3 = await getJSON('./out/word-jlpt3.json');
+const word2 = await getJSON('./out/word-jlpt2.json');
+const word1 = await getJSON('./out/word-jlpt1.json');
+// old
+const kanji5old = await getJSON('../../src/data/jlpt5-kanjis.json');
+const kanji4old = await getJSON('../../src/data/jlpt4-kanjis.json');
+const kanji3old = await getJSON('../../src/data/jlpt3-kanjis.json');
+const kanji2old = await getJSON('../../src/data/jlpt2-kanjis.json');
+const kanji1old = await getJSON('../../src/data/jlpt1-kanjis.json');
+const word5old = await getJSON('../../src/data/jlpt5-words.json');
+const word4old = await getJSON('../../src/data/jlpt4-words.json');
+const word3old = await getJSON('../../src/data/jlpt3-words.json');
+const word2old = await getJSON('../../src/data/jlpt2-words.json');
+const word1old = await getJSON('../../src/data/jlpt1-words.json');
 
 const set = {
-	kanjis: [kanji5, kanji4, kanji3, kanji2, kanji1],
-	words: [word5, word4, word3, word2, word1],
+	kanjis: [kanji5, kanji4, kanji3, kanji2, kanji1] as Kanji[][],
+	words: [word5, word4, word3, word2, word1] as Word[][],
 };
 
 const oldset = {
@@ -42,7 +70,7 @@ for (; i < 5; ++i) {
 	console.log(`data length: ${data.length}`);
 	console.log(`old data length: ${oldData.length}`);
 
-	const dataKanjis = data.map((i) => i.name);
+	const dataKanjis = data.map((i) => i.v);
 	const oldDataKanjis = oldData.map((i) => i[1]) as string[];
 
 	console.log('Kanjis that are not in old data:');
@@ -51,7 +79,7 @@ for (; i < 5; ++i) {
 	);
 	for (const k of kanjisThatAreNotInNewData) {
 		const otherJlpt = set.kanjis.findIndex((kanjiSet) =>
-			kanjiSet.some((i) => i.name === k)
+			kanjiSet.some((i) => i.v === k)
 		);
 		console.log(`${k}${otherJlpt >= 0 ? ` (in jlpt ${5 - otherJlpt})` : ''}`);
 	}
@@ -62,4 +90,3 @@ for (; i < 5; ++i) {
 	console.log();
 	console.log();
 }
-*/
