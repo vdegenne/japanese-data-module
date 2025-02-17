6090050b5612d07ed4586752b822164db54a942e
Valentin Degenne <vdegenne@gmail.com>
2025-02-17 20:00:12 +0100

    3.0.1

--- packages/japanese-data-module/src/build/constants.ts
+++ packages/japanese-data-module/src/build/constants.ts
@@ -1,14 +1,14 @@
 import path from 'path';
-import kanji5 from '@jdm-internal/raw/kanji5.json' assert {type: 'json'};
-import kanji4 from '@jdm-internal/raw/kanji4.json' assert {type: 'json'};
-import kanji3 from '@jdm-internal/raw/kanji3.json' assert {type: 'json'};
-import kanji2 from '@jdm-internal/raw/kanji2.json' assert {type: 'json'};
-import kanji1 from '@jdm-internal/raw/kanji1.json' assert {type: 'json'};
-import word5 from '@jdm-internal/raw/word5.json' assert {type: 'json'};
-import word4 from '@jdm-internal/raw/word4.json' assert {type: 'json'};
-import word3 from '@jdm-internal/raw/word3.json' assert {type: 'json'};
-import word2 from '@jdm-internal/raw/word2.json' assert {type: 'json'};
-import word1 from '@jdm-internal/raw/word1.json' assert {type: 'json'};
+import kanji5 from '@jdm-internal/raw/kanji5.json' with {type: 'json'};
+import kanji4 from '@jdm-internal/raw/kanji4.json' with {type: 'json'};
+import kanji3 from '@jdm-internal/raw/kanji3.json' with {type: 'json'};
+import kanji2 from '@jdm-internal/raw/kanji2.json' with {type: 'json'};
+import kanji1 from '@jdm-internal/raw/kanji1.json' with {type: 'json'};
+import word5 from '@jdm-internal/raw/word5.json' with {type: 'json'};
+import word4 from '@jdm-internal/raw/word4.json' with {type: 'json'};
+import word3 from '@jdm-internal/raw/word3.json' with {type: 'json'};
+import word2 from '@jdm-internal/raw/word2.json' with {type: 'json'};
+import word1 from '@jdm-internal/raw/word1.json' with {type: 'json'};
 import {type Kanji, type Word} from '@jdm-internal/raw';
 
 export const _TARGET_PATH = path.join('lib', 'data');
